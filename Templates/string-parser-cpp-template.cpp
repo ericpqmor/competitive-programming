12120f012dbfc50530b212312981bd562a8490a4
Eric Moreira <ericpqmor@gmail.com>
2018-03-12 09:47:11 -0300

    Adding string parser template

---
+++ Templates/string-parser-cpp-template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	string line;
+	
+	//Reads the string until end of line
+	while(getline(std::cin, line)) {
+	
+	        //Get string stream and separate string, word by word
+		istringstream iss(line);
+		do {
+			string word;
+			iss >> word;
+ 
+		} while(iss);
+		cout << endl;
+	}
+
+	return 0;
+}
